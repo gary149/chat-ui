f9d1ff8d36646183827c554b5de4639845d39832
Victor Muštar <victor.mustar@gmail.com>
2025-09-09 17:35:42 +0200

    Revert "Replace ScrollToPreviousBtn with UserMessageNav"
    This reverts commit 149a9dccb5f4d929b96a9968767bbd6aa9030b73.

--- src/lib/components/ScrollToBottomBtn.svelte
+++ src/lib/components/ScrollToBottomBtn.svelte
@@ -41,7 +41,7 @@
 	<button
 		transition:fade={{ duration: 150 }}
 		onclick={() => scrollNode.scrollTo({ top: scrollNode.scrollHeight, behavior: "smooth" })}
-		class="btn absolute flex size-9 rounded-full border border-black/5 bg-white/10 text-gray-500 shadow-sm backdrop-blur-sm transition-all dark:border-white/5 dark:bg-white/5 dark:hover:bg-gray-600 {className}"
-		><IconChevron /></button
+		class="btn absolute flex h-[41px] w-[41px] rounded-full border bg-white shadow-md transition-all hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:shadow-gray-950 dark:hover:bg-gray-600 {className}"
+		><IconChevron classNames="mt-[2px]" /></button
 	>
 {/if}
